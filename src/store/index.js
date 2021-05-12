c39f3c182d66d4382227f38f26688a60d4ec8789
Ramazan <laumanov.r@gmail.com>
2021-05-12 13:28:53 +0600

    modified store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import {account} from './account.module';
-import {role} from './role.module';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
-		account,
-		role
+		account
 	}
 });
