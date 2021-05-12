00424b4ea1856bc7395d92a8e6fc7bafb1c74833
Ramazan <laumanov.r@gmail.com>
2021-05-12 13:31:04 +0600

    remade account store

--- src/store/account.module.js
+++ src/store/account.module.js
@@ -1,12 +1,12 @@
 import {UserService} from '../services/user.service';
+import {RoleService} from '../services/role.service';
 import router from '../router/index';
 
 const user = JSON.parse(window.localStorage.getItem('ihsanUser'));
 
 const state = {
 	user: user || {},
-	onError: '',
-	onSuccess: ''
+	onError: ''
 };
 
 const actions = {
@@ -14,7 +14,19 @@ const actions = {
 		try {
 			const user = await UserService.login(body);
 			window.localStorage.setItem('ihsanUser', JSON.stringify(user));
+			dispatch('getUserPermissions', user.roles[0].id);
+		} catch (err) {
+			commit('setError', err);
+		}
+	},
+	async getUserPermissions({commit}, roleId) {
+		try {
+			const res = await RoleService.fetchRoleById(roleId);
+			const user = JSON.parse(window.localStorage.getItem('ihsanUser'));
+			user.permissions = res.permissions;
+			user.roleTitle = res.title;
 			commit('setUser', user);
+			window.localStorage.setItem('ihsanUser', JSON.stringify(user));
 			router.push('/user-manage');
 		} catch (err) {
 			commit('setError', err);
