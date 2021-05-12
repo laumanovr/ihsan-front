ed8936c0e04dc5e8f1313c4ffc2d383795909bbd
Ramazan <laumanov.r@gmail.com>
2021-05-12 12:28:20 +0600

    include import in main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import VModal from 'vue-js-modal';
+import VueToast from 'vue-toast-notification';
+import '@/assets/style/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueToast, {position: 'top', duration: 2500});
+Vue.use(VModal);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
+}).$mount('#app');
