27bc50208ac410d9d6c016053ea628a148247061
Ramazan <laumanov.r@gmail.com>
2021-05-21 11:34:41 +0600

    modified main js file

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import VModal from 'vue-js-modal';
 import VueToast from 'vue-toast-notification';
+import PreLoader from '@/components/general/PreLoader';
 import '@/assets/style/main.scss';
 
 Vue.config.productionTip = false;
 Vue.use(VueToast, {position: 'top', duration: 2500});
 Vue.use(VModal);
+Vue.component('PreLoader', PreLoader);
 
 new Vue({
 	router,
