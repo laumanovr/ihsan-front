abe6df188a10910d025afe566feeafdfc0304b6d
Ramazan <laumanov.r@gmail.com>
2021-05-21 11:36:43 +0600

    add api for user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,4 @@
-import {API_BASE_URL, sendPostRequest} from './api.service';
+import {API_BASE_URL, sendGetRequest, sendPostRequest} from './api.service';
 
 export class UserService {
 	constructor() {}
@@ -6,5 +6,35 @@ export class UserService {
 	static login(body) {
 		const url = `${API_BASE_URL}/auth/login`;
 		return sendPostRequest(url, body);
+	}
+
+	static createUser(body) {
+		const url = `${API_BASE_URL}/user/create`;
+		return sendPostRequest(url, body);
+	}
+
+	static updateUser(body) {
+		const url = `${API_BASE_URL}/user/edit/${body.id}`;
+		return sendPostRequest(url, body);
+	}
+
+	static fetchUserList() {
+		const url = `${API_BASE_URL}/user/list`;
+		return sendGetRequest(url);
+	}
+
+	static fetchByDepartment(departId) {
+		const url = `${API_BASE_URL}/user/list/department/${departId}`;
+		return sendGetRequest(url);
+	}
+
+	static findById(userId) {
+		const url = `${API_BASE_URL}/user/${userId}`;
+		return sendGetRequest(url);
+	}
+
+	static fetchMyProfile() {
+		const url = `${API_BASE_URL}/user/me`;
+		return sendGetRequest(url);
 	}
 }
