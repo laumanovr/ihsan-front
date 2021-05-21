30ee7882b585bc19e0c62861ec03b9c7fcb4b47c
Ramazan <laumanov.r@gmail.com>
2021-05-21 11:41:39 +0600

    add api for role service

--- src/services/role.service.js
+++ src/services/role.service.js
@@ -3,6 +3,11 @@ import {API_BASE_URL, sendGetRequest, sendPostRequest} from './api.service';
 export class RoleService {
 	constructor() {}
 
+	static fetchAllRoles() {
+		const url = `${API_BASE_URL}/security/role/list`;
+		return sendGetRequest(url);
+	}
+
 	static fetchRoleById(id) {
 		const url = `${API_BASE_URL}/security/role/${id}`;
 		return sendGetRequest(url);
@@ -10,6 +15,11 @@ export class RoleService {
 
 	static createRole(body) {
 		const url = `${API_BASE_URL}/security/role/create`;
+		return sendPostRequest(url, body);
+	}
+
+	static updateRole(body) {
+		const url = `${API_BASE_URL}/security/role/edit/${body.id}`;
 		return sendPostRequest(url, body);
 	}
 }
