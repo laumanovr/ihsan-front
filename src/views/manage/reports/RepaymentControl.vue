ec85fbfd1d166a6a757f1e141dc7ba84560ef828
Ramazan <laumanov.r@gmail.com>
2021-10-04 14:21:15 +0600

    create RepaymentControl report

---
+++ src/views/manage/reports/RepaymentControl.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="repay-control-container">
+        <div class="head-title d-flex align-center">
+            <img src="../../../assets/icons/arrow-dark.svg" @click="$router.go(-1)" class="back">
+            <span>Отчет Контроль-погашений</span>
+        </div>
+
+        <div class="d-flex align-center justify-center">
+            <div class="masked-input">
+                <span>Дата от</span>
+                <MaskedInput
+                        mask="11.11.1111"
+                        placeholder="ДД.ММ.ГГГГ"
+                        v-model="startDate"
+                />
+            </div>
+            <div class="masked-input date-to">
+                <span>Дата до</span>
+                <MaskedInput
+                        mask="11.11.1111"
+                        placeholder="ДД.ММ.ГГГГ"
+                        v-model="endDate"
+                />
+            </div>
+            <button class="btn blue-primary" @click="getRepayControlReport">Фильтр</button>
+        </div>
+
+        <div class="d-flex justify-center">
+            <table class="table" v-if="reportList.length">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Филиал</th>
+                    <th>Программа</th>
+                    <th>Кол-во</th>
+                    <th>План</th>
+                    <th>Оплачено</th>
+                    <th>Разница</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(item, i) in reportList" :key="i">
+                    <td>{{i + 1}}</td>
+                    <td>{{item.departmentTitle}}</td>
+                    <td>{{programType[item.programType]}}</td>
+                    <td>{{item.totalCount}}</td>
+                    <td>{{countPlan(item)}}</td>
+                    <td>{{countPaid(item)}}</td>
+                    <td>{{countPlan(item) - countPaid(item)}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-center" v-if="isEmpty">
+            <h3>Пусто</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ReportService} from '../../../services/report.service';
+import {format} from 'date-fns';
+
+export default {
+	components: {
+		MaskedInput: () => import('vue-masked-input')
+	},
+	data() {
+		return {
+			isLoading: false,
+			reportList: [],
+			startDate: '',
+			endDate: '',
+			isEmpty: false,
+			programType: {
+				'0': 'Авто',
+				'1': 'Жилье'
+			}
+		};
+	},
+	created() {
+		const date = new Date();
+		this.startDate = format(date.setMonth(date.getMonth() - 3), 'dd.MM.yyyy');
+		this.endDate = format(new Date(), 'dd.MM.yyyy');
+	},
+	methods: {
+		async getRepayControlReport() {
+			try {
+				this.isLoading = true;
+				this.reportList = await ReportService.fetchRepaymentControl(this.startDate, this.endDate);
+				this.isEmpty = !this.reportList.length;
+				this.isLoading = false;
+			} catch (err) {
+				this.$toast.error(err);
+				this.isLoading = false;
+			}
+		},
+
+		countPlan(item) {
+			return (item.totalLoanTerm * item.totalPayment).toFixed(2);
+		},
+
+		countPaid(item) {
+			return (item.totalLoanAmount - item.totalPaid).toFixed(2);
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+    .repay-control-container {
+        .masked-input {
+            &.date-to {
+                margin: 0 15px 0;
+            }
+            input {
+                background: #fff;
+            }
+        }
+        table.table {
+            max-width: 100%;
+            table-layout: auto;
+
+            th, td {
+                white-space: nowrap;
+            }
+        }
+    }
+</style>
